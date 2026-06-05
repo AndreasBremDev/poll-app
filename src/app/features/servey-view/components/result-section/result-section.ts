ae019a886b2e3657a47d8374a3bf22b2e550df4b
Andreas Brem <andi.brem@web.de>
2026-06-05 08:58:52 +0200

    added new sections and routes to servey-view and servey-create. added routerlink at soon-section.

---
+++ src/app/features/servey-view/components/result-section/result-section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-result-section',
+  imports: [],
+  templateUrl: './result-section.html',
+  styleUrl: './result-section.scss',
+})
+export class ResultSection {}
---
+++ src/app/features/servey-view/components/view-section/view-section.ts
@@ -0,0 +1,29 @@
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Supabase } from '../../../../shared/services/supabase';
+import { Poll } from '../../../../shared/interfaces/interface';
+
+@Component({
+  selector: 'app-view-section',
+  imports: [],
+  templateUrl: './view-section.html',
+  styleUrl: './view-section.scss',
+})
+export class ViewSection {
+
+  supabase = inject(Supabase);
+  route = inject(ActivatedRoute);
+
+  currentPoll = signal<Poll | undefined>(undefined);
+
+  async ngOnInit(): Promise<void> {
+    if (this.supabase.polls().length === 0) {
+      await this.supabase.getData();
+    }
+    let currentId = Number(this.route.snapshot.paramMap.get('id'));
+    let foundPoll = this.supabase.polls().find(poll => poll.id === currentId);
+    this.currentPoll.set(foundPoll);
+  }
+
+
+}
