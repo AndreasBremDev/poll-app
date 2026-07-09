82f385c8eb107cb70f97fdbacd3e57f354ffc937
Andreas Brem <andi.brem@web.de>
2026-07-09 17:06:11 +0200

    -feat: icons added -feat: form (label/input) added, -feat: typescript  form and validators added

--- src/app/features/survey-create/survey-create.ts
+++ src/app/features/survey-create/survey-create.ts
@@ -1,13 +1,39 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ModalDialog } from '../../shared/services/modal-dialog';
+import { FormBuilder, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
+import { Supabase } from '../../shared/services/supabase';
 
 @Component({
   selector: 'app-survey-create',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './survey-create.html',
   styleUrl: './survey-create.scss',
 })
 export class SurveyCreate {
 
   modalDialog = inject(ModalDialog)
+
+  selectedCategory = signal<string>('all');
+
+  formbuilder = inject(FormBuilder)
+
+  supabase = inject(Supabase)
+
+  userform = this.formbuilder.group({
+    title: ['', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(100)
+    ]],
+    enddate: ['', []],
+    description: ['']
+  })
+
+  onChangeCategory(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const value = selectElement.value;
+    this.selectedCategory.set(value);
+    selectElement.blur();
+  }
+
 }
